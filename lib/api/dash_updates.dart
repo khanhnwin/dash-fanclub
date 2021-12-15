8a61ac6e896479522a8b4eb9cbef24059deec7b7
Khanh Nguyen <khanhnwin@gmail.com>
2021-12-15 10:17:47 -0600

    MyDash v1

---
+++ lib/api/dash_updates.dart
@@ -0,0 +1,96 @@
+class DashUpdates {
+  final List _dashUpdates = [
+    {
+      "id": 0,
+      "author": "Dash the Flutter Mascot",
+      "image": "https://docs.flutter.dev/assets/images/dash/dash-fainting.gif",
+      "content":
+          "Hey y'all, this is Dash here! I'm finally back from vacation and ready to get Fluttering again!",
+      "authorImage": "https://docs.flutter.dev/assets/images/dash/Dash.png",
+      "comments": [
+        {
+          "author": "Khanh Nguyen",
+          "comment": "Welcome back Dash!!",
+        },
+        {
+          "author": "Dash the Flutter Mascot",
+          "comment": "Thanks Khanh!!",
+        },
+      ],
+    },
+    {
+      "id": 1,
+      "author": "Khanh Nguyen",
+      "image":
+          "https://docs.flutter.dev/assets/images/dash/dash-conference-swag.jpg",
+      "content":
+          "Y'all, I've found the Dash plushie warehouse. No one tell Shams that I'm here... ",
+      "authorImage":
+          "https://yt3.ggpht.com/ytc/AKedOLSepRT0gjLvp3HSmlNpdM7GHfwmBj8Cegc1s0mWKQ=s176-c-k-c0x00ffffff-no-rj",
+      "comments": [
+        {
+          "author": "Shams Zakhour",
+          "comment": "...I can see this post you know...",
+        },
+        {
+          "author": "Khanh Nguyen",
+          "comment": "...Ooopsies 😳 ",
+        }
+      ],
+    },
+    {
+      "id": 2,
+      "author": "Craig Labenz",
+      "image": "",
+      "content":
+          "I would give anything to know how elevators influence my dog's understanding of the world",
+      "authorImage":
+          "https://pbs.twimg.com/profile_images/1290813376377974784/bdzo7QcN_400x400.jpg",
+      "comments": [
+        {
+          "author": "Craig's Dog",
+          "comment": "WOOF!",
+        }
+      ],
+    },
+    {
+      "id": 3,
+      "author": "Shams Zakhour",
+      "image":
+          "https://docs.flutter.dev/assets/images/dash/ShamsDashJacket.png",
+      "content": "Check out my new Dash-inspired jean jacket! – Shams",
+      "authorImage":
+          "https://docs.flutter.dev/assets/images/dash/dash-prototypes.jpg",
+      "comments": [
+        {
+          "author": "Nilay Yener",
+          "comment": "I love it!",
+        },
+        {
+          "author": "Khanh Nguyen",
+          "comment":
+              "This is my very really super long comment that is going to need to wrap around my name and then keep going on the next row. They’re two separate text fields because I want to style them differently."
+        }
+      ],
+    },
+  ];
+
+  List addComment(int postId, String comment, String author) {
+    _dashUpdates[postId]["comments"]
+        .add({author: "Khanh Nguyen", comment: comment});
+
+    return _dashUpdates;
+  }
+
+  Map updateContent(Map content) {
+    // TODO: Add some content validation here
+
+    _dashUpdates.add(content);
+
+    return content;
+  }
+
+  List getUpdates() {
+    return _dashUpdates;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'ui/home_feed.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,39 +9,41 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Dasher',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.lightBlue,
-      ),
-      home: const MyHomePage(title: 'Dasher'),
+          colorScheme: const ColorScheme(
+            primary: Color.fromRGBO(0, 169, 224, 1.0),
+            primaryVariant: Color.fromRGBO(0, 119, 200, 1.0),
+            secondary: Color.fromRGBO(29, 79, 145, 1.0),
+            secondaryVariant: Color.fromRGBO(198, 218, 231, 1.0),
+            error: Color(0xb71c1cff),
+            surface: Color.fromRGBO(255, 255, 255, 1.0),
+            onPrimary: Color.fromRGBO(255, 255, 255, 1.0),
+            background: Color.fromRGBO(198, 218, 231, 1.0),
+            onSecondary: Color.fromRGBO(255, 255, 255, 1.0),
+            onError: Color.fromRGBO(255, 255, 255, 1.0),
+            brightness: Brightness.light,
+            onBackground: Color(0x000000FF),
+            onSurface: Color(0x000000FF),
+          ),
+          textTheme: GoogleFonts.robotoTextTheme(
+            Theme.of(context).textTheme,
+          ),
+          appBarTheme: AppBarTheme(
+            titleTextStyle: GoogleFonts.patuaOne(
+              fontSize: 28,
+            ),
+          )),
+      home: const MyHomePage(title: 'MyDash'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,97 +52,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List _dashUpdates = [
-    {
-      "author": "Dash the Flutter Mascot",
-      "image": "https://flutter.dev/assets/images/dash/dash-fainting.gif",
-      "content":
-          "Hey y'all, this is Dash here! I'm finally back from vacation and ready to get Fluttering again!",
-      "authorImage": "https://flutter.dev/assets/images/dash/Dash.png",
-    },
-    {
-      "author": "Khanh Nguyen",
-      "image":
-          "https://flutter.dev/assets/images/dash/dash-conference-swag.jpg",
-      "content":
-          "Y'all, I've found the Dash plushie warehouse. No one tell Shams that I'm here...",
-      "authorImage":
-          "https://yt3.ggpht.com/ytc/AKedOLSepRT0gjLvp3HSmlNpdM7GHfwmBj8Cegc1s0mWKQ=s176-c-k-c0x00ffffff-no-rj",
-    },
-    {
-      "author": "Shams Zakhour",
-      "image": "https://flutter.dev/assets/images/dash/ShamsDashJacket.png",
-      "content": "Check out my new Dash-inspired jean jacket! - Shams."
-    },
-  ];
-
-  bool dashUpdatesAvailable = true;
-
-  Widget dashUpdates() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: _dashUpdates.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Column(
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: _dashUpdates[index]["authorImage"] != null
-                        ? CircleAvatar(
-                            backgroundImage: NetworkImage(
-                                _dashUpdates[index]["authorImage"]),
-                            backgroundColor: Colors.lightBlue,
-                          )
-                        : CircleAvatar(
-                            backgroundColor: Colors.brown.shade800,
-                            child: const Text("SZ"),
-                          ),
-                  ),
-                  Text(
-                    _dashUpdates[index]["author"],
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-              const Padding(padding: EdgeInsets.all(4)),
-              Text(_dashUpdates[index]["content"]),
-              const Padding(padding: EdgeInsets.all(8)),
-              Image(image: NetworkImage(_dashUpdates[index]["image"])),
-              const Padding(padding: EdgeInsets.all(16), child: Divider()),
-            ],
-          );
-        });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title, style: const TextStyle(color: Colors.white)),
         iconTheme: const IconThemeData(color: Colors.white),
       ),
-      drawer: Drawer(
-        child: ListView(padding: EdgeInsets.zero, children: const [
-          DrawerHeader(
-            child: Text('Drawer Header'),
-          )
-        ]),
-      ),
       bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.lightBlue,
+        selectedItemColor: Theme.of(context).colorScheme.primary,
         unselectedItemColor: Colors.blueGrey[400],
         unselectedLabelStyle: TextStyle(color: Colors.blueGrey[400]),
-        showUnselectedLabels: true,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: 0,
+        type: BottomNavigationBarType.fixed,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -162,38 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: dashUpdatesAvailable
-            ? dashUpdates()
-            : Column(
-                // Column is also a layout widget. It takes a list of children and
-                // arranges them vertically. By default, it sizes itself to fit its
-                // children horizontally, and tries to be as tall as its parent.
-                //
-                // Invoke "debug painting" (press "p" in the console, choose the
-                // "Toggle Debug Paint" action from the Flutter Inspector in Android
-                // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-                // to see the wireframe for each widget.
-                //
-                // Column has various properties to control how it sizes itself and
-                // how it positions its children. Here we use mainAxisAlignment to
-                // center the children vertically; the main axis here is the vertical
-                // axis because Columns are vertical (the cross axis would be
-                // horizontal).
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const <Widget>[
-                  Text(
-                    'No new updates from Dash!',
-                  ),
-                  Padding(
-                      padding: EdgeInsets.all(0),
-                      child:
-                          Image(image: AssetImage('assets/images/dash.png'))),
-                ],
-              ),
-      ),
+      body: const HomeFeed(),
     );
   }
 }
